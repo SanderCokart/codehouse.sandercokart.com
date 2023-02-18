d201f1845093061efeab733154e4f9771501945f
Sander Cokart <cokart32@gmail.com>
2023-02-18 16:16:47 +0100

    Fixed navigation, color problems

--- lib/layouts/RootLayout/Header.tsx
+++ lib/layouts/RootLayout/Header.tsx
@@ -3,11 +3,15 @@ import {ThemeToggle} from '@/components/ThemeToggle';
 import {DesktopNavigation} from '@/layouts/RootLayout';
 
 export default function Header() {
+    //git rename master main
+    //git branch -m master main
+    //git rename remote branch master to main
+    //git push origin -u main
     return (
         <section
-            className="mx-auto flex max-w-7xl items-center overflow-hidden p-1 text-3xl font-bold text-white font-[LetsGoDigital] md:justify-between">
+            className="mx-auto flex max-w-7xl items-center overflow-hidden p-1 text-3xl font-bold text-white font-[LetsGoDigital] justify-evenly md:justify-between">
             <h1>
-                <a className="relative flex flex-col items-end leading-none drop-shadow-lg group" href="#hero">
+                <a className="flex flex-col items-end leading-none drop-shadow-lg group" href="#hero">
                     <span className="block transition-colors group-hover:text-secondaryLight">Sander's CodeHouse</span>
                     <span className="block text-lg transition-colors">Let's Code...</span>
                 </a>
--- lib/layouts/RootLayout/Navigation/MobileNavigation.tsx
+++ lib/layouts/RootLayout/Navigation/MobileNavigation.tsx
@@ -3,23 +3,23 @@ import {FaHome, FaSpa, FaComment, FaEnvelope} from 'react-icons/fa';
 export default function MobileNavigation() {
     return (
         <nav aria-label="mobile"
-             className="flex md:hidden fixed bottom-0 left-0 w-full bg-primary dark:bg-primary font-bold">
-            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:accentLight dark:hover:bg-accentLight"
+             className="flex md:hidden fixed bottom-0 left-0 w-full bg-primaryLight dark:bg-primaryDark font-bold text-white">
+            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:bg-primaryLight dark:hover:bg-primaryLight"
                href="#hero">
                 <FaHome className="text-xl"/>
                 <span className="text-xs">Home</span>
             </a>
-            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:accentLight dark:hover:bg-accentLight"
+            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:bg-primaryLight dark:hover:bg-primaryLight"
                href="#techstack">
                 <FaSpa className="text-xl"/>
                 <span className="text-xs">Tech Stack</span>
             </a>
-            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:accentLight dark:hover:bg-accentLight"
+            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:bg-primaryLight dark:hover:bg-primaryLight"
                href="#testimonials">
                 <FaComment className="text-xl"/>
                 <span className="text-xs">Testimonials</span>
             </a>
-            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:accentLight dark:hover:bg-accentLight"
+            <a className="flex flex-col items-center gap-1 w-full py-2 text-center hover:bg-primaryLight dark:hover:bg-primaryLight"
                href="#contact">
                 <FaEnvelope className="text-xl"/>
                 <span className="text-xs">Contact Us</span>
