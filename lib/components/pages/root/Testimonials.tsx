c362841ca645236b1a862033bc7f9d2d182737a6
Sander Cokart <cokart32@gmail.com>
2023-02-26 00:24:17 +0100

    Fixed overflow scroll on Testimonials.tsx

--- lib/components/pages/root/Testimonials.tsx
+++ lib/components/pages/root/Testimonials.tsx
@@ -36,7 +36,7 @@ function Quote({ children, author }: QuoteProps) {
                        transition={{ opacity: { duration: 1.5 }, scale: { duration: .5 } }}
                        viewport={{ once: true, margin: isMobile ? '0px' : '-33%' }}
                        whileInView={{ opacity: 1, scale: 1 }}>
-            <motion.div className="p-2" viewport={{ margin: '-45%' }} whileInView={{ scale: 1.1 }}>
+            <motion.div className="m-4 md:m-0" viewport={{ margin: '-45%' }} whileInView={{ scale: 1.1 }}>
                 <blockquote className="rounded-3xl transition-colors bg-primary dark:bg-transparent">
                     <p className="relative before:absolute before:h-8 md:before:h-14 before:content-['\201C'] before:font-serif before:grid before:place-items-start before:text-7xl before:md:text-9xl before:top-2 md:before:top-0 before:left-2 md:before:left-0 before:text-secondaryLight after:absolute after:h-8 md:after:h-14 after:content-['\201D'] after:font-serif after:grid after:place-items-end after:text-7xl after:md:text-9xl after:bottom-2 md:after:bottom-0 after:right-2 md:after:right-0 after:text-secondaryLight">
                     <span className="block p-8 italic font-bold text-center md:text-left md:p-12 text-left text-2xl text-white md:text-3xl">
